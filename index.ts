10cf370712514fa898b24ecc3e8f49970c2240f3
teppeis <teppeis@gmail.com>
2015-01-21 22:41:34 +0900

    Incremental compile

--- index.ts
+++ index.ts
@@ -8,71 +8,107 @@ import path = require('path');
 import ts = require('typescript');
 
 var FILENAME_TS = 'file.ts';
- 
-/**
- * @param {string} code TypeScript source code to compile
- * @param {ts.CompilerOptions=} options TypeScript compile options (some options are ignored)
- */
-function compile(code: string, options: ts.CompilerOptions = {}) {
-    if (options.target == null) {
-        options.target = ts.ScriptTarget.ES3;
-    }
-    if (options.module == null) {
-        options.module = ts.ModuleKind.None;
-    }
 
-    var outputs: {[index: string]: string} = {};
-    var compilerHost = {
-        getSourceFile: function(filename: string, languageVersion: ts.ScriptTarget) {
-            if (filename === FILENAME_TS) {
-                return ts.createSourceFile(filename, code, languageVersion, '0');
-            } else if (/^lib(?:\.es6)?\.d\.ts$/.test(filename)) {
-                var libPath = path.join(path.dirname(require.resolve('typescript')), filename);
-                var libSource = fs.readFileSync(libPath).toString();
-                return ts.createSourceFile(filename, libSource, languageVersion, '0');
-            }
-            return undefined;
-        },
-        writeFile: function(name: string, text: string, writeByteOrderMark: boolean) {
-            outputs[name] = text;
-        },
-        getDefaultLibFilename: function(options: ts.CompilerOptions) {
-            if (options.target === ts.ScriptTarget.ES6) {
-                return 'lib.es6.d.ts';
-            } else {
-                return 'lib.d.ts';
-            }
-        },
-        useCaseSensitiveFileNames: function() { return true; },
-        getCanonicalFileName: function(filename: string) { return filename; },
-        getCurrentDirectory: function() { return ''; },
-        getNewLine: function() { return os.EOL; }
-    };
+class TypeScriptSimple {
 
-    var program = ts.createProgram([FILENAME_TS], options, compilerHost);
-    var diagnostics = program.getDiagnostics();
-    if (diagnostics.length > 0) {
-        throw new Error(formatDiagnostics(diagnostics));
-    }
+    service: ts.LanguageService = null;
+    outputs: ts.Map<string> = {};
+    options: ts.CompilerOptions;
+    files: ts.Map<{version: number; text: string;}> = {};
 
-    var checker = program.getTypeChecker(true);
-    diagnostics = checker.getDiagnostics();
-    checker.emitFiles();
-    if (diagnostics.length > 0) {
-        throw new Error(formatDiagnostics(diagnostics));
-    }
-
-    return outputs[FILENAME_TS.replace(/ts$/, 'js')];
-}
-
-function formatDiagnostics(diagnostics: ts.Diagnostic[]) {
-    return diagnostics.map((d) => {
-        if (d.file) {
-            return 'L' + d.file.getLineAndCharacterFromPosition(d.start).line + ': ' + d.messageText;
-        } else {
-            return d.messageText;
+    /**
+     * @param {ts.CompilerOptions=} options TypeScript compile options (some options are ignored)
+     */
+    constructor(options: ts.CompilerOptions = {}) {
+        if (options.target == null) {
+            options.target = ts.ScriptTarget.ES3;
         }
-    }).join(os.EOL);
+        if (options.module == null) {
+            options.module = ts.ModuleKind.None;
+        }
+        this.options = options;
+    }
+ 
+    /**
+     * @param {string} code TypeScript source code to compile
+     * @return {string}
+     */
+    compile(code: string) {
+        if (!this.service) {
+            this.service = this.createService();
+        }
+
+        var file = this.files[FILENAME_TS];
+        file.text = code;
+        file.version++;
+
+        return this.toJavaScript(this.service);
+    }
+
+    getTypeScriptBinDir() {
+        return path.dirname(require.resolve('typescript'));
+    }
+
+    createService() {
+        var defaultLib = this.getDefaultLibFilename(this.options);
+        var defaultLibPath = path.join(this.getTypeScriptBinDir(), defaultLib);
+        this.files[defaultLib] = { version: 0, text: fs.readFileSync(defaultLibPath).toString() };
+        this.files[FILENAME_TS] = { version: 0, text: '' };
+
+        var servicesHost: ts.LanguageServiceHost = {
+            getScriptFileNames: () => [this.getDefaultLibFilename(this.options), FILENAME_TS],
+            getScriptVersion: (filename) => this.files[filename] && this.files[filename].version.toString(),
+            getScriptSnapshot: (filename) => {
+                var file = this.files[filename];
+                return {
+                    getText: (start, end) => file.text.substring(start, end),
+                    getLength: () => file.text.length,
+                    getLineStartPositions: () => [],
+                    getChangeRange: (oldSnapshot) => undefined
+                };
+            },
+            getCurrentDirectory: () => process.cwd(),
+            getScriptIsOpen: () => true,
+            getCompilationSettings: () => this.options,
+            getDefaultLibFilename: (options: ts.CompilerOptions) => {
+                return this.getDefaultLibFilename(options);
+            },
+            log: (message) => console.log(message)
+        };
+
+        return ts.createLanguageService(servicesHost, ts.createDocumentRegistry())
+    }
+
+    getDefaultLibFilename(options: ts.CompilerOptions) {
+        if (options.target === ts.ScriptTarget.ES6) {
+            return 'lib.es6.d.ts';
+        } else {
+            return 'lib.d.ts';
+        }
+    }
+
+    toJavaScript(service: ts.LanguageService) {
+        var output = service.getEmitOutput(FILENAME_TS);
+        if (output.emitOutputStatus === ts.EmitReturnStatus.Succeeded) {
+            var filename = FILENAME_TS.replace(/ts$/, 'js');
+            return output.outputFiles.filter((file) => file.name === filename)[0].text.replace(/\r\n/g, os.EOL);
+        }
+
+        var allDiagnostics = service.getCompilerOptionsDiagnostics()
+            .concat(service.getSyntacticDiagnostics(FILENAME_TS))
+            .concat(service.getSemanticDiagnostics(FILENAME_TS));
+        throw new Error(this.formatDiagnostics(allDiagnostics));
+    }
+
+    formatDiagnostics(diagnostics: ts.Diagnostic[]) {
+        return diagnostics.map((d) => {
+            if (d.file) {
+                return 'L' + d.file.getLineAndCharacterFromPosition(d.start).line + ': ' + d.messageText;
+            } else {
+                return d.messageText;
+            }
+        }).join(os.EOL);
+    }
 }
 
-export = compile;
+export = TypeScriptSimple;
--- test/test.js
+++ test/test.js
@@ -3,32 +3,96 @@ var fs = require('fs');
 var path = require('path');
 
 var ts = require('typescript');
-var tss = require('../');
+var TypeScriptSimple = require('../');
 
-describe('typescript-simple', function() {
-    it('compiles correct code', function() {
-        var src = "var x: number = 1;";
-        var expected = 'var x = 1;\n';
-        assert.equal(tss(src), expected);
+describe('typescript-update', function() {
+    var tss;
+    context('default target (ES5)', function() {
+        beforeEach(function() {
+            tss = new TypeScriptSimple();
+        });
+
+        it.skip('bench', function() {
+            var src = "var x: number = 1;";
+            var expected = 'var x = 1;\n';
+            var start = new Date().getTime();
+            for (var i = 0; i < 10; i++) {
+                tss = new TypeScriptSimple();
+                tss.compile(src);
+            }
+            console.log('each time compile', (new Date() - start)/1000);
+
+            start = new Date().getTime();
+            for (var i = 0; i < 10; i++) {
+                new TypeScriptSimple();
+                tss.compile(src);
+            }
+            console.log('incremental compile', (new Date() - start)/1000);
+        });
+
+        it('compiles correct code', function() {
+            var src = "var x: number = 1;";
+            var expected = 'var x = 1;\n';
+            assert.equal(tss.compile(src), expected);
+        });
+
+        it('does not replace CRLF literal', function() {
+            var src = "var x: string = '\\r\\n';";
+            var expected = "var x = '\\r\\n';\n";
+            assert.equal(tss.compile(src), expected);
+        });
+
+        it('compiles many times', function() {
+            var src = "var x: number = 1;";
+            var expected = 'var x = 1;\n';
+            assert.equal(tss.compile(src), expected);
+
+            src = "var y: number = 2;";
+            expected = 'var y = 2;\n';
+            assert.equal(tss.compile(src), expected);
+
+            src = "var z: number = 3;";
+            expected = 'var z = 3;\n';
+            assert.equal(tss.compile(src), expected);
+        });
+
+        it('throws an error for a type error', function() {
+            var src = "var x: number = 'str';";
+            assert.throws(function() {
+                tss.compile(src);
+            }, /^Error: L1: Type 'string' is not assignable to type 'number'./);
+        });
+
+        it('throws an error for ES6 "let"', function() {
+            var src = "let x: number = 1;";
+            assert.throws(function() {
+                tss.compile(src);
+            }, /^Error: L1: 'let' declarations are only available when targeting ECMAScript 6 and higher./);
+        });
+
+        it('throws an error for ES6 Promise', function() {
+            var src = "var x = new Promise(function (resolve, reject) {\n});";
+            assert.throws(function() {
+                tss.compile(src);
+            }, /^Error: L1: Cannot find name 'Promise'./);
+        });
     });
 
-    it('throws an error', function() {
-        var src = "var x: number = 'str';";
-        assert.throws(function() {
-            tss(src);
-        }, /^Error: L1: Type 'string' is not assignable to type 'number'./);
-    });
+    context('target ES6', function() {
+        beforeEach(function() {
+            tss = new TypeScriptSimple({target: ts.ScriptTarget.ES6});
+        });
 
-    it('compile ES6 feature', function() {
-        var src = "let x: number = 1;";
-        var expected = 'let x = 1;\n';
-        assert.equal(tss(src, {target: ts.ScriptTarget.ES6}), expected);
-    });
+        it('compiles ES6 "let"', function() {
+            var src = "let x: number = 1;";
+            var expected = 'let x = 1;\n';
+            assert.equal(tss.compile(src), expected);
+        });
 
-    it('throws an error for ES6 feature with non ES6 target', function() {
-        var src = "let x: number = 1;";
-        assert.throws(function() {
-            tss(src);
-        }, /^Error: L1: 'let' declarations are only available when targeting ECMAScript 6 and higher./);
+        it('compiles ES6 Promise', function() {
+            var src = "var x = new Promise(function (resolve, reject) {\n});";
+            var expected = src + '\n';
+            assert.equal(tss.compile(src), expected);
+        });
     });
 });
